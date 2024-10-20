7168e609602d4a6d4edf1c227c7e9e75c0994809
Md Sharique <md21_ug@cse.nits.ac.in>
2024-10-20 23:23:58 +0530

    first commit of react series

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Item from './components/Item';
+import ItemDate from './components/ItemDate';
 function App() {
+  const response=[
+    {
+      ItemName: "Nirma",
+      ItemDay: "12th",
+      ItemMonth: "July",
+      ItemYear: "2001"
+    },
+    {
+      ItemName: "surg excel",
+      ItemDay: "12th",
+      ItemMonth: "July",
+      ItemYear: "2001"
+    },
+    {
+      ItemName: "honda",
+      ItemDay: "12th",
+      ItemMonth: "june",
+      ItemYear: "1993"
+    }
+  ];
   return (
+    <>
+    <ItemDate day={response[0].ItemDay} month={response[0].ItemMonth} year={response[0].ItemYear}></ItemDate>
+    <Item name={response[0].ItemName}></Item>
+    <ItemDate day={response[1].ItemDay} month={response[1].ItemMonth} year={response[1].ItemYear}></ItemDate>
+    <Item name={response[1].ItemName}></Item>
+    <ItemDate day={response[2].ItemDay} month={response[2].ItemMonth} year={response[2].ItemYear}></ItemDate>
+    <Item name={response[2].ItemName}></Item>
+    
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
+    </>
   );
 }
 
